932d46d4bb8a383ed47611ee1fc3a7faada1d7c0
root <root@localhost.localdomain>
2025-11-26 15:32:44 +0800

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main()
+{
+    println!("Guess the number 😊");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your number:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("Your guess: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("Equal");
+                break;
+            },
+        }
+    }
+}
